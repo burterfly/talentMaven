bd196fb201e4d88a13f827d436763d414d421225
伏击 <3465907793@qq.com>
2020-02-09 18:29:24 +0800

    能从数据库取到数据，不能加载在html

---
+++ src/main/java/com/talent/entity/usedtalent.java
@@ -0,0 +1,114 @@
+package com.talent.entity;
+
+public class usedtalent {
+    private Integer tid;
+
+    public Integer getTid() {
+        return tid;
+    }
+
+    public void setTid(Integer tid) {
+        this.tid = tid;
+    }
+
+    public String getTname() {
+        return tname;
+    }
+
+    public void setTname(String tname) {
+        this.tname = tname;
+    }
+
+    public Integer getTage() {
+        return tage;
+    }
+
+    public void setTage(Integer tage) {
+        this.tage = tage;
+    }
+
+    public String getTsex() {
+        return tsex;
+    }
+
+    public void setTsex(String tsex) {
+        this.tsex = tsex;
+    }
+
+    public String getTcer() {
+        return tcer;
+    }
+
+    public void setTcer(String tcer) {
+        this.tcer = tcer;
+    }
+
+    public String getTedu() {
+        return tedu;
+    }
+
+    public void setTedu(String tedu) {
+        this.tedu = tedu;
+    }
+
+    public String getTpro() {
+        return tpro;
+    }
+
+    public void setTpro(String tpro) {
+        this.tpro = tpro;
+    }
+
+    public Integer getTsco() {
+        return tsco;
+    }
+
+    public void setTsco(Integer tsco) {
+        this.tsco = tsco;
+    }
+
+    public String getTtime() {
+        return ttime;
+    }
+
+    public void setTtime(String ttime) {
+        this.ttime = ttime;
+    }
+
+    public String getTother() {
+        return tother;
+    }
+
+    public void setTother(String tother) {
+        this.tother = tother;
+    }
+
+    private String tname;
+    private Integer tage;
+    private String tsex;
+    private String tcer;
+    private String tedu;
+    private String tpro;
+    private Integer tsco;
+    private String ttime;
+    private String tother;
+
+    public String getEtime() {
+        return etime;
+    }
+
+    public void setEtime(String etime) {
+        this.etime = etime;
+    }
+
+    public String getEsco() {
+        return esco;
+    }
+
+    public void setEsco(String esco) {
+        this.esco = esco;
+    }
+
+    private String etime;
+    private String esco;
+}
